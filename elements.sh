00a2ea70d8daefa2273410cd186510cb4ac11d18
Shantanu Vedak <vedaksv@protonmail.com>
2024-07-22 14:22:10 +0530

    reads arguements and categorises them

--- elements.sh
+++ elements.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/bash
 PSQL="--username=postgres --dbname=periodic_table --port=5433 -t --no-align -c"
+if [[ -z $1 ]]
+then
+    echo Please provide an element as an arguement.
+fi
 
-echo Please provide an element as an arguement: 
+if [[ $1 =~ ^[0-9]+ ]]
+then
+    echo atomic no
+elif [[ $1 =~ ^[A-Za-z]+ ]]
+then
+    echo name or symbol
+fi
